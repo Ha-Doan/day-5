c88a59afffc2850a600b77da4909a865e673fb0a
Ha-Doan <doanthanhha@gmail.com>
2017-11-03 21:40:15 +0100

    Add fies

---
+++ app/models/dinosaur.rb
@@ -0,0 +1,13 @@
+class Dinosaur < ApplicationRecord
+  # remove the self.order_by_name method
+  validates :name, presence: true
+  validates :name, uniqueness: true
+
+  validates :age, length: { maximum: 5 }, allow_blank: true
+  validates :image_url, length: { maximum: 255 }, allow_blank: true
+  validates :image_url, presence: true
+  scope :order_by_name, -> { order(:name) }
+  def is_baby?
+    age < 3
+  end
+end
